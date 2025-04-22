7543223d1d801e09e1b819c027909917572e6f4f
basnt ebrahim <161939675+BasntEbrahim7@users.noreply.github.com>
2025-04-22 20:28:01 +0200

    Update storage.js

--- storage.js
+++ storage.js
@@ -12,13 +12,25 @@ function getBooks(){} // Retrieves the list of all books from localStorage
 
 function saveBooks(books){}// Saves the list of books to localStorage
 
-function getCurrentUser(){}// Retrieves the current logged-in user's data from localStorage
+function getCurrentUser(){
+    return JSON.parse(localStorage.getItem("currentUser"));
 
-function saveCurrentUser(user){}// Saves the current logged-in user's data to localStorage
 
-function getUsers(){}// Retrieves the list of all users from localStorage
+}// Retrieves the current logged-in user's data from localStorage
 
-function saveUsers(users){}// Saves the list of all users to localStorage
+function saveCurrentUser(user){
+    localStorage.setItem("currentUser", JSON.stringify(user));
+}
+
+function getUsers(){
+    const users = localStorage.getItem("users");
+    return users ? JSON.parse(users) : [];
+}// Retrieves the list of all users from localStorage
+
+function saveUsers(users){
+    localStorage.setItem("users", JSON.stringify(users));
+
+}// Saves the list of all users to localStorage
 
 function getBorrowedBooks(){}// Retrieves the list of borrowed books for the current user from localStorage
 
@@ -34,7 +46,7 @@ function getBookStatus(bookid){
     let books = getBooks();
     let book = books.find(b=>b.id === bookid);
     if(!book){
-       return false
+        return false
     }
     return book.isBorrowed ? true : false;
 }
