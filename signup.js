681bde8bb913b253f67e0fc82504d853826b690a
basnt ebrahim <161939675+BasntEbrahim7@users.noreply.github.com>
2025-04-22 20:26:12 +0200

    signup.js

---
+++ signup.js
@@ -0,0 +1,38 @@
+const form = document.getElementById('signup-form');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value.trim();
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+
+    if (password !== confirmPassword) {
+        alert('Password does not match!');
+        return;
+    }
+
+    const users = getUsers();
+
+    // تأكد من عدم تكرار الإيميل
+    if (users.find(u => u.email === email)) {
+        alert("Email already exists!");
+        return;
+    }
+
+    const userData = {
+        email: email,
+        username: username,
+        password: password
+    };
+
+    users.push(userData);
+    saveUsers(users);
+
+    // saveCurrentUser كـ object
+    const currentUser = { email: email, isAdmin: false };
+    saveCurrentUser(currentUser);
+
+    window.location.href = "dashboard.html";
+});
