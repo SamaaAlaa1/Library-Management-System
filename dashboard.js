18942a6ac94839c0cb5e4ad50e128c9453bccd4b
basnt ebrahim <161939675+BasntEbrahim7@users.noreply.github.com>
2025-04-25 01:58:25 +0300

    dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,10 +1,8 @@
 const currentUser = getCurrentUser();
 
 if (currentUser && currentUser.isAdmin) {
-    const btns = document.querySelectorAll(".borrow-btn");
-    btns.forEach(btn => {
-        btn.innerHTML = "EDIT DETAILS";
-    });
 
-    document.querySelector(".navbar").innerHTML += '<a href="/AddBook.html">ADD BOOK</a>';
+
+    document.querySelector(".navbar").innerHTML += '<a href="adminDashboard.html"> ADD& EDIT BOOK </a>';
 }
+
