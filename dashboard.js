ebd0184f9b4886ad4d35b42d868d2f468fbe0efd
zeinab-mohamed0 <zeinabmohamed0101@gmail.com>
2025-04-25 16:33:08 +0300

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -6,3 +6,10 @@ if (currentUser && currentUser.isAdmin) {
     document.querySelector(".navbar").innerHTML += '<a href="adminDashboard.html"> ADD& EDIT BOOK </a>';
 }
 
+let btn = document.getElementsByClassName("fav-btn")
+for(let i = 0; i < btn.length; i++){
+    btn[i].onclick = function()
+{
+    this.classList.toggle("fav")
+}
+}
