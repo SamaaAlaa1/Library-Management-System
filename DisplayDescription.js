3b4c2a6de39e991b6c93c9774395ec982137d3fc
bassant960 <163014114+bassant960@users.noreply.github.com>
2025-04-25 22:30:43 +0300

    Add files via upload

---
+++ DisplayDescription.js
@@ -0,0 +1,29 @@
+function sveBookID(bookid){
+    localStorage.setItem("selectedBookID", bookid);
+    window.location.href("BookDetails.html");
+}
+function displayDescription(containerid){
+    const selectedid = localStorage.getItem("selectedBookID");
+    if(!selectedid){
+        document.getElementById(containerid).innerHTML= "No book is selected";
+        return;
+    }
+    let books = [];
+    for(let i=0; i<localStorage.length(); i++){
+        let value = JSON.parse(localStorage.getItem(localStorage.key(i)));
+        if(Array.isArray(value)){
+            books = value;
+            break;
+        }
+
+    }
+    const book = books.find(b=>b.id === selectedid);
+    if(book){
+        book.getElementById("bookimg").src = book.image;
+        document.getElementById("booktitle").textContent = book.title
+        document.getElementById("genere").textContent = book.genere;
+        document.getElementById("bookdetails").textContent= book.description;
+    }else{
+        document.getElementById(containerid).innerHTML="Not Found";
+    }
+}
